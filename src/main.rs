65b82e2ddf9b2a653fc67f40aaf32b53fa52aff0
Denis Katkov <Denis.Katkov@standardbank.co.za>
2021-02-09 20:18:51 +0300

    simple append

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,50 @@
+use std::cell::RefCell;
+use std::rc::Rc;
+
+type SingleLink = Option<Rc<RefCell<Node>>>;
+
+struct Node {
+    value: String,
+    next: SingleLink
+}
+
+impl Node {
+    fn new(value: String) -> Rc<RefCell<Node>> {
+        Rc::new(RefCell::new(Node {
+            value: value,
+            next: None
+        }))
+    }
+}
+
+struct TransactionLog {
+    head: SingleLink,
+    tail: SingleLink,
+    pub length: u64
+}
+
+impl TransactionLog {
+    pub fn new_empty() -> TransactionLog {
+        TransactionLog {
+            head: None,
+            tail: None,
+            length: 0
+        }
+    }
+
+    pub fn append(&mut self, value: String) {
+        let new = Node::new(value);
+        match self.tail.take() {
+            Some(old) => old.borrow_mut().next = Some(new.clone()),
+            None => self.head = Some(new.clone())
+        };
+        self.length += 1;
+        self.tail = Some(new);
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    let mut log = TransactionLog::new_empty();
+    log.append(String::from("qwerty"));
+    println!("{}", log.length);
 }
