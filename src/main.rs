ad5052adcd8cb60240b4e7ccc0bc1ae2d5c48a19
dkatkov-moon <katkov.dk@pg.com>
2021-02-14 21:01:52 +0300

    best transaction log temp

--- src/main.rs
+++ src/main.rs
@@ -5,141 +5,63 @@ type Link = Option<Rc<RefCell<Node>>>;
 
 #[derive(Debug)]
 struct Node {
-    value: String,
-    next: Link,
-    prev: Link,
+    next: Vec<Link>,
+    pub offset: u64,
+    pub command: String,
 }
 
 impl Node {
-    fn new(value: String) -> Rc<RefCell<Node>> {
+    fn new(links: Vec<Link>, offset: u64, command: String) -> Rc<RefCell<Node>> {
         Rc::new(RefCell::new(Node {
-            value: value,
-            next: None,
-            prev: None,
+            next: links,
+            offset: offset,
+            command: command,
         }))
     }
 }
 
-pub struct ListIterator {
-    current: Link,
-}
-
-impl ListIterator {
-    fn new(start_at: Link) -> ListIterator {
-        ListIterator { current: start_at }
-    }
-}
-
-impl Iterator for ListIterator {
-    type Item = String;
-    fn next(&mut self) -> Option<String> {
-        let current = &self.current;
-        let mut result = None;
-        self.current = match current {
-            Some(ref current) => {
-                let current = current.borrow();
-                result = Some(current.value.clone());
-                current.next.clone()
-            }
-            None => None,
-        };
-        result
-    }
-}
-
-impl DoubleEndedIterator for ListIterator {
-    fn next_back(&mut self) -> Option<String> {
-        let current = &self.current;
-        let mut result = None;
-        self.current = match current {
-            Some(ref current) => {
-                let current = current.borrow();
-                result = Some(current.value.clone());
-                current.prev.clone()
-            }
-            None => None,
-        };
-        result
-    }
-}
-
 #[derive(Debug, Clone)]
-struct BetterTransactionLog {
+struct BestTransactionLog {
     head: Link,
-    tail: Link,
+    tails: Vec<Link>,
+    max_level: usize,
     pub length: u64,
 }
 
-impl IntoIterator for BetterTransactionLog {
-    type Item = String;
-    type IntoIter = ListIterator;
-
-    fn into_iter(self) -> Self::IntoIter {
-        ListIterator::new(self.head)
-    }
-}
-
-impl BetterTransactionLog {
-    pub fn new_empty() -> BetterTransactionLog {
-        BetterTransactionLog {
+impl BestTransactionLog {
+    pub fn new_empty(max_level: usize) -> BestTransactionLog {
+        BestTransactionLog {
+            max_level: max_level,
             head: None,
-            tail: None,
+            tails: vec![None; max_level + 1],
             length: 0,
         }
     }
 
-    pub fn append(&mut self, value: String) {
-        let new = Node::new(value);
-        match self.tail.take() {
-            Some(old) => {
-                old.borrow_mut().next = Some(new.clone());
-                new.borrow_mut().prev = Some(old);
-            }
-            None => self.head = Some(new.clone()),
+    pub fn append(&mut self, offset: u64, command: String) {
+        let level = 1 + if self.head.is_none() {
+            self.max_level
+        } else {
+            self.get_level()
         };
-        self.length += 1;
-        self.tail = Some(new);
-    }
 
-    pub fn pop(&mut self) -> Option<String> {
-        self.head.take().map(|head| {
-            if let Some(next) = head.borrow_mut().next.take() {
-                next.borrow_mut().prev = None;
-                self.head = Some(next);
-            } else {
-                self.tail.take();
+        let new = Node::new(vec![None; level], offset, command);
+        for i in 0..level {
+            if let Some(old) = self.tails[i].take() {
+                let next = &mut old.borrow_mut().next;
+                next[i] = Some(new.clone());
             }
-
-            self.length -= 1;
-            Rc::try_unwrap(head)
-                .ok()
-                .expect("Something is terribly wrong")
-                .into_inner()
-                .value
-        })
+            self.tails[i] = Some(new.clone());
+        }
     }
 
-    pub fn iter(&self) -> ListIterator {
-        ListIterator::new(self.head.clone())
-    }
-
-    pub fn back_iter(&self) -> ListIterator {
-        ListIterator::new(self.tail.clone())
+    fn get_level(&self) -> usize {
+        let mut n = 0;
+        while rand::random::<bool>() && self.max_level < n {
+            n += 1;
+        }
+        n
     }
 }
 
-fn main() {
-    let mut transaction_log = BetterTransactionLog::new_empty();
-    transaction_log.append(String::from("first"));
-    transaction_log.append(String::from("second"));
-    transaction_log.append(String::from("third"));
-    println!("length={}", transaction_log.length);
-
-    let mut tr_iter = transaction_log.into_iter();
-    println!("{:#?}", tr_iter.next());
-    println!("{:#?}", tr_iter.next());
-    println!("{:#?}", tr_iter.next());
-    println!("{:#?}", tr_iter.next_back());
-    println!("{:#?}", tr_iter.next_back());
-    println!("{:#?}", tr_iter.next_back());
-}
+fn main() {}
